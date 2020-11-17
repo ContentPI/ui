254da2b395f925d01622b9eeacd70f907e39a153
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2020-11-16 22:50:22 -0400

    remove old emotion dependencies and update tsconfig

--- src/@types/emotion.d.ts
+++ src/@types/emotion.d.ts
@@ -1,15 +1,7 @@
 // import original module declarations
 import '@emotion/react'
-import '@emotion/styled'
 
-// import theme from '../theme'
-
-// export type ThemeType = typeof theme
-
-// declare module '@emotion/react' {
-//   export interface Theme extends ThemeType {}
-// }
-import { Breakpoints } from './createBreakpoints'
+import { Breakpoints } from '../theme/createBreakpoints'
 
 // and extend them!
 interface IBackground {
@@ -33,18 +25,6 @@ interface ITextColors {
 }
 
 declare module '@emotion/react' {
-  export interface Theme {
-    breakpoints: Breakpoints
-    palette: {
-      background: IBackground
-      primary: IPalette
-      secondary: IPalette
-      text: ITextColors
-    }
-  }
-}
-
-declare module '@emotion/styled' {
   export interface Theme {
     breakpoints: Breakpoints
     palette: {
